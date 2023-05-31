65fbabd57726ce67c71d0aacc485deefe928937c
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2023-05-31 18:06:35 +0700

    deklarasi variabel dan memberi nilai yg aksesnya bersifat private pada class remoteLampu

--- program enkapsulasi/program enkapsulasi.cpp
+++ program enkapsulasi/program enkapsulasi.cpp
@@ -2,3 +2,14 @@
 using namespace std;
 
 class remoteLampu {
+private:
+	string saklarNo[10];
+public:
+	void seSaklarNo(int i, string value) {
+		saklarNo[i] = value;
+	}
+	string getSaklarNo(int i) {
+		return saklarNo[i];
+	}
+};
+

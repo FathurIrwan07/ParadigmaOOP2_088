29ce45b819cc33f850b1d47c99963ebb8a2e4a10
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2023-05-31 16:55:30 +0700

    manambahkan 2 methode dalam class AbstraksiKlas yg aksesnya bersifat public

--- ProgramAbstraksi/ProgramAbstraksi.cpp
+++ ProgramAbstraksi/ProgramAbstraksi.cpp
@@ -4,6 +4,20 @@ using namespace std;
 class AbstraksiKlas {
 private: string x, y;
 
+public:
+
+	//metode untuk mengisi nilai
+	//private member
+	void setXY(string a, string b) {
+		x = a;
+		y = b;
+	}
+	//menampilkan nilai
+	void display() {
+		cout << "x = " << x << endl;
+		cout << "y = " << y << endl;
+	}
+};
 
 
 
